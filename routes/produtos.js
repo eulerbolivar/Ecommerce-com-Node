658099a1229f7d28c4fd50d4b2100c19708ac69d
Euler Oliveira <euleragenda21@gmail.com>
2023-01-29 15:19:56 -0300

    Adicionando GET único

--- routes/produtos.js
+++ routes/produtos.js
@@ -39,7 +39,18 @@ router.post('/', (req, res, next) => {
 
 // RETORNA OS DADOS DE UM PRODUTO ESPECÍFICO
 router.get('/:id_produto', (req, res, next) => {
-
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({error: error})}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    });
+});
 
 // ALTERA UM PRODUTO
 router.patch('/', (req, res, next) => {
